ee6ba5260fc612e945c3a00ef75b57f418ed75e9
connorrr81 <52404310+connorrr81@users.noreply.github.com>
2022-04-04 22:10:43 +0100

    Amendments to the Linear Regression

--- FitModels.py
+++ FitModels.py
@@ -6,13 +6,17 @@ Created on Fri Sep 24 12:11:59 2021
 """
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import re
 import os 
 import time
+import statsmodels.api as sm
 from datetime import timedelta
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
+from statsmodels.api import OLS
 
 
 path = os.path.dirname(os.path.realpath(__file__))
@@ -27,7 +31,7 @@ onehot=True
 train_df = model_df[model_df["Date"]<"2021-07-25 00:00:00"]
 test_df = model_df[model_df["Date"]>"2021-07-24 00:00:00"]
 
-# Remove heats and semi final swims
+# Remove heats and semi final swims at the olympics
 if onehot:
     test_df.drop_duplicates(subset = test_df.columns.difference(["Date", "Time (s)", "Month", "Age at Swim", "PB at Swim"]),
                             keep = 'first', inplace = True)
@@ -36,18 +40,35 @@ else:
                             keep = 'first', inplace = True)
 
 
-X_train = train_df.iloc[:,3:]
+X_train = train_df.iloc[:,2:]
 y_train = train_df.iloc[:,1]
-X_test = test_df.iloc[:,3:]
+X_test = test_df.iloc[:,2:]
 y_test = test_df.iloc[:,1]
 
-#%% Fit the linear model
+#%% Missing valyes imputation
+model_df.isnull().sum().sort_values(axis=0) # 318 in time since COVID column
+
+imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+model_df["Time (s) change since COVID"] = imp.fit_transform(model_df[["Time (s) change since COVID"]]).ravel()
+
+
+#%% Linear Regression
+# Make sure assumptions are met e.g. shapiro-wilk test, heteroskedasticity
+
+# Fit the linear model
 model = LinearRegression()
 model.fit(X_train,y_train)
 
 y_pred = model.predict(X_test)
+residuals = y_pred - y_test
 
 print(mean_squared_error(y_test, y_pred))
+
+#print(OLS(y_train, X_train).fit().summary())
+
+#produce residual plot
+plt.scatter(residuals,y_pred)
+plt.show()
 
 #%% Fit the random forest
 classifier = RandomForestRegressor(n_estimators=100, criterion='mse', random_state=1)
