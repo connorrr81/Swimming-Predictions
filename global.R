2ef03254f2a4daebd8f4a1b95f47f15b21535efc
connorrr81 <52404310+connorrr81@users.noreply.github.com>
2022-04-27 20:17:18 +0100

    Delete global.R

--- global.R
+++
@@ -1,9 +0,0 @@
-library(shiny)
-library(dplyr)
-library(bslib)
-library(rsconnect)
-
-data <- read.csv("predicted_times.csv")
-
-data <- data %>% rename(Time = Time..s.,
-                        Time_Predicted = value)
